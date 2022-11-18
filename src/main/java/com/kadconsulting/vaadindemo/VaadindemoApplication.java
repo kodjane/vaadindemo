3bee31869fd7aff70c32aed6c7523277bf66d95b
kadmindset <kodjane09@hotmail.com>
2022-11-18 11:58:22 +0100

    first commit

---
+++ src/main/java/com/kadconsulting/vaadindemo/VaadindemoApplication.java
@@ -0,0 +1,13 @@
+package com.kadconsulting.vaadindemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaadindemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VaadindemoApplication.class, args);
+    }
+
+}
