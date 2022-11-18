0c65d7a4dbebd7970993f9729e4918aa84dfcca4
kadmindset <kodjane09@hotmail.com>
2022-11-18 14:10:58 +0100

    implement views and configure user authentification

--- src/main/java/com/kadconsulting/vaadindemo/VaadindemoApplication.java
+++ src/main/java/com/kadconsulting/vaadindemo/VaadindemoApplication.java
@@ -1,13 +1,41 @@
 package com.kadconsulting.vaadindemo;
 
+import com.kadconsulting.vaadindemo.ui.LoginView;
+import com.vaadin.flow.spring.security.VaadinWebSecurity;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 
 @SpringBootApplication
-public class VaadindemoApplication {
+public class VaadindemoApplication extends VaadinWebSecurity {
 
     public static void main(String[] args) {
         SpringApplication.run(VaadindemoApplication.class, args);
     }
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+        setLoginView(http, LoginView.class);
+    }
+
+    @Bean
+    public UserDetailsManager userDetailsService() {
+        UserDetails user =
+                User.withUsername("kadminset")
+                        .password("{noop}admin")
+                        .roles("ADMIN")
+                        .build();
+        UserDetails admin =
+                User.withUsername("admin")
+                        .password("{noop}admin")
+                        .roles("ADMIN")
+                        .build();
+        return new InMemoryUserDetailsManager(user, admin);
+    }
 }
