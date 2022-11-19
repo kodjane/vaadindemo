008ec8302050ff4f4dc6363885f94ca5e55a6b3e
kadmindset <kodjane09@hotmail.com>
2022-11-19 09:54:43 +0100

    Add new view for adding a new book, create manual binder for testing purpose

--- src/main/java/com/kadconsulting/vaadindemo/ui/AdminView.java
+++ src/main/java/com/kadconsulting/vaadindemo/ui/AdminView.java
@@ -5,6 +5,7 @@ import com.kadconsulting.vaadindemo.backend.BookService;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.RouterLink;
 import org.vaadin.crudui.crud.impl.GridCrud;
 
 import javax.annotation.security.RolesAllowed;
@@ -16,6 +17,8 @@ public class AdminView extends VerticalLayout {
         GridCrud<Book> crud = new GridCrud<>(Book.class, bookService);
         crud.getGrid().setColumns("title","published","rating");
         crud.getCrudFormFactory().setVisibleProperties("title","published","rating");
+        crud.setAddOperationVisible(true);
+        crud.getCrudLayout().addToolbarComponent(new RouterLink("New Book", NewBookView.class));
         add(
                 new H1("Admin view"),
                 crud
